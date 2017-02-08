91740894359842a4cdc7f143be6dcf72df859df0
Byron <byrontribs@yahoo.com>
2017-02-08 22:05:36 +0200

    added unreal project---L-creating and deleting landscapes

---
+++ Zank/Source/Zank/ZankGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ZankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZANK_API AZankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
